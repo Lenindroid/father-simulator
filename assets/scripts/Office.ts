b3873a8362a6d68e8e7449eb8bd5bb6522c69db3
Lenin Rojas Sánchez <leninrojassanchez09@gmail.com>
2025-04-22 20:25:17 -0600

    First version of assets for the office

---
+++ assets/scripts/Office.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Office')
+export class Office extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
